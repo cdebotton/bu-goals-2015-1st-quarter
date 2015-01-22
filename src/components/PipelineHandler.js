b191c8a38c468d9cee4a4f34db4df2dc8a481c7a
Christian de Botton <debotton@brooklynunited.com>
2015-01-22 16:18:24 -0500

    Done with copy.

--- src/components/PipelineHandler.js
+++ src/components/PipelineHandler.js
@@ -9,7 +9,10 @@ var PipelineHandler = React.createClass({
   render() {
     return (
       <div className="pipeline-handler">
-        <h2>PipelineHandler</h2>
+        <h3>Improve our pipeline for development and deployment.</h3>
+        <p>Deployment of projects is still kind of a messy hassle. Without dev&ndash;ops, it&apos;s particularly important to have a clean pipeline for development and deployment. We already started the development side of this by using a semi-standardized gulp pipeline for our builds. This quarter we want to make deployment as straight forward as possible.</p>
+        <p>The goal for the quarter to deploy all new projects on a DigitalOcean instance, which should be purchased and set up to use before development starts. This will allow us to properly set up dev, staging, and production environments, and make launching sites as simple as repointing the DNS.</p>
+        <p><strong>Bonus:</strong> Build a CLI tool that handles deployments.</p>
       </div>
     );
   }
--- src/components/ToolsHandler.js
+++ src/components/ToolsHandler.js
@@ -9,7 +9,9 @@ var ToolsHandler = React.createClass({
   render() {
     return (
       <div className="tools-handler">
-        <h2>ToolsHandler</h2>
+        <h3>Release tools for the community.</h3>
+        <p>To establish recognition in the community as an expert in the field, we need to contribute to the community. Creating open source tools for the community to use and contribute to is extremely important. The goal for this quarter is to release at least one tool for the community with a site to support it. We have several tools being worked on, including an isomorphic server, a build tool pipeline, and a styleguide editor.</p>
+        <p><strong>Bonus:</strong> Release all three.</p>
       </div>
     );
   }
